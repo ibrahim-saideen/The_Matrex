42861136fa2ba506baf0dbe4990b29f0b1342a5d
ibrahim saideen <saideenibrahim@gmail.com>
2026-06-29 09:35:55 -0700

    k

--- ex1/loading.py
+++ ex1/loading.py
@@ -1,28 +1,50 @@
+from importlib.metadata import version
+import sys
+
 
 print('LOADING STATUS: Loading programs...')
 print('Checking dependencies:\n')
-
+x = False
 
 try:
     import pandas
-    print(f'[OK] pandas {pandas.__version__} '
+    print(f'[OK] pandas {version('pandas')} '
           '- Data manipulation ready')
 except Exception:
-    print('[KO] can not import importlib '
-          'install it first (ya 7arfosh) \n'
-          'use:pip install')
-
-
+    print('[KO] pandas not installed yet')
+    x = True
 
 try:
-    from importlib.metadata import version
-    print(f'[OK] importlib {version('importlib')} '
-          '- Data manipulation ready')
+    import numpy
+    print(f'[OK] numpy {version('numpy')} '
+          '- Numerical computation ready')
 except Exception:
-    print('[KO] can not import importlib '
-          'install it first (ya 7arfosh) '
-          'pip install -r ')
+    print('[KO] numpy not installed yet')
+    x = True
 
+try:
+    import matplotlib.pyplot as plt
+    print(f'[OK] matplotlib {version('matplotlib')} '
+          '- Visualization ready')
+except Exception:
+    print('[KO] matplotlib not installed yet')
+    x = True
 
+if x:
+    print('\nInstall all the required packages:')
+    print('use one of:')
+    print('pip install -r requirements.txt')
+    print('poetry install')
+    sys.exit()
 
+print('Analyzing Matrix data...')
+print('Processing 1000 data points...')
+print('Generating visualization...')
 
+data = numpy.random.randint(0, 100, size=1000)
+df = pandas.DataFrame(data, columns=['signal'])
+plt.plot(df['signal'])
+plt.savefig("data.png")
+
+print('Analysis complete!')
+print('Results saved to: data.png')
